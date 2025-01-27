ef2b9046207bc0654ed245bae7f09cf6d536f7f2
samarthmore078 <samarthmore369@gmail.com>
2025-01-27 19:43:22 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+   <div>
+    <header>TS Amazona</header>
+    <main>All right reserved</main>
+    <footer></footer>
+   </div>
+  )
+}
+
+export default App
